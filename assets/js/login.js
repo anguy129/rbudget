47d1a8fe087600d3ebc726792330d0f67bddf439
JacquesFr <jfrac001@ucr.edu>
2019-10-22 16:57:14 -0700

    created login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,8 @@
 function login() {
-	email = document.getElementById("signUp_email").value;
-	password = document.getElementById("signUp_password0").value;
-	confirmPw = document.getElementById("signUp_password1").value;
+	email = document.getElementById("email").value;
+	password = document.getElementById("password").value;
 	
-	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
