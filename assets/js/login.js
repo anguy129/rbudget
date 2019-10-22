932d996ec71bd35f5c2586f903b5008d74e6e844
JacquesFr <jfrac001@ucr.edu>
2019-10-22 16:42:37 -0700

    created login.js

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+function login() {
+	email = document.getElementById("signUp_email").value;
+	password = document.getElementById("signUp_password0").value;
+	confirmPw = document.getElementById("signUp_password1").value;
+	
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // ...
+	});
+};
