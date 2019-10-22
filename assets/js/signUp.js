f42abc67858a54f6d81cd7a47f5102a0e16e30bb
eric-Is-Me <chaingisme@gmail.com>
2019-10-22 16:16:27 -0700

    Successfully signing up proper users

--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,4 +1,19 @@
 /* Create new password based user on Firebase */
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyB2qbMEfgBqH98Pdbmm3kYPzTTyip0la3U",
+	authDomain: "rbudget-a870b.firebaseapp.com",
+	databaseURL: "https://rbudget-a870b.firebaseio.com",
+	projectId: "rbudget-a870b",
+	storageBucket: "rbudget-a870b.appspot.com",
+	messagingSenderId: "893639759319",
+	appId: "1:893639759319:web:c9e9cb3c47d609eed26792",
+	measurementId: "G-JZDK060R21"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function signUp() {
 	email = document.getElementById("signUp_email").value;
 	password = document.getElementById("signUp_password0").value;
@@ -8,12 +23,13 @@ function signUp() {
 		alert("Passwords don't match");
 	}
 	else {
-		alert("success");
 		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 	  		// Handle Errors here.
 	  		var errorCode = error.code;
 	  		var errorMessage = error.message;
+	  		alert("ERROR");
 	  		// ...
 		});
+		alert("success");
 	}
 };
