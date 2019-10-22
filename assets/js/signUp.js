0068ce23f5812377b246b2b471821b21752c1fad
eric-Is-Me <chaingisme@gmail.com>
2019-10-22 16:35:12 -0700

    testing redirect

--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,19 +1,5 @@
 /* Create new password based user on Firebase */
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-	apiKey: "AIzaSyB2qbMEfgBqH98Pdbmm3kYPzTTyip0la3U",
-	authDomain: "rbudget-a870b.firebaseapp.com",
-	databaseURL: "https://rbudget-a870b.firebaseio.com",
-	projectId: "rbudget-a870b",
-	storageBucket: "rbudget-a870b.appspot.com",
-	messagingSenderId: "893639759319",
-	appId: "1:893639759319:web:c9e9cb3c47d609eed26792",
-	measurementId: "G-JZDK060R21"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 function signUp() {
 	email = document.getElementById("signUp_email").value;
 	password = document.getElementById("signUp_password0").value;
@@ -27,9 +13,8 @@ function signUp() {
 	  		// Handle Errors here.
 	  		var errorCode = error.code;
 	  		var errorMessage = error.message;
-	  		alert("ERROR");
 	  		// ...
 		});
-		alert("success");
+		window.location.replace("https://anguy129.github.io/rbudget/homepage.html?");
 	}
 };
