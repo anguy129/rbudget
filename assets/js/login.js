8058b95cb7945043548f2d2e932f8630b2c0347f
JacquesFr <jfrac001@ucr.edu>
2019-10-22 19:14:03 -0700

    fixed sign in code

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,24 @@
 function login() {
 
-	var email = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
-
+	var email = document.getElementById("email_login").value;
+	var password = document.getElementById("password_login").value;
+	var errors = 0;
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 	  // ...
-	  window.alert("Error: " errorMessage);
+	  window.alert("Error : " + errorMessage);
 	});
+	if(!errorMessage){
+		window.location.replace("https://anguy129.github.io/rbudget/homepage.html");
+	}
+
+	else{
+		window.location.replace("https://anguy129.github.io/rbudget/signIn.html");
+	}
 };
+
+function logout(){
+  firebase.auth().signOut();
+}
