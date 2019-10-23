a5505ed70b06b1f82f0f4f116627a9f5c0174453
JacquesFr <jfrac001@ucr.edu>
2019-10-22 18:07:34 -0700

    added error windowpage for login still testing

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,13 @@
 function login() {
-	email = document.getElementById("email").value;
-	password = document.getElementById("password").value;
-	
+
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 	  // ...
+	  window.alert("Error: " errorMessage);
 	});
 };
