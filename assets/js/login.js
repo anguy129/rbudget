fa5b2ecaa2ba1bc6bcbf4a9dd85e385a6d839571
JacquesFr <jfrac001@ucr.edu>
2019-10-22 19:30:03 -0700

    testing online

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,21 +2,29 @@ function login() {
 
 	var email = document.getElementById("email_login").value;
 	var password = document.getElementById("password_login").value;
-	var errors = 0;
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 	  // ...
 	  window.alert("Error : " + errorMessage);
+	  
 	});
-	if(!errorMessage){
-		window.location.replace("https://anguy129.github.io/rbudget/homepage.html");
-	}
+	if(error){
+        window.location.replace("https://anguy129.github.io/rbudget/homepage.html?");
 
-	else{
-		window.location.replace("https://anguy129.github.io/rbudget/signIn.html");
-	}
+	  }
+	  else{
+	  	window.location.replace("https://anguy129.github.io/rbudget/homepage.html?");
+
+	  }
+	/*var user = firebase.auth().currentUser;
+    if (user) {
+        console.log(user);
+    } else {
+    	window.location.replace("https://anguy129.github.io/rbudget/signIn.html?");
+
+    }*/
 };
 
 function logout(){
