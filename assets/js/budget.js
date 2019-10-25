03e6d7e5361d4f5f7a2f3bb54eb94f61bad4ca0e
ASHTM15 <amcda005@ucr.edu>
2019-10-24 17:01:00 -0700

    added save db stuff

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,9 +1,10 @@
 
 
 const form = document.querySelector('#main_budget');
-
+var docRef =  db.collection("main_budget").doc("latest"); 
+/*
 function setBudget(){
-	var budgetSet = document.getElementById("budget").value;
+	//var budgetSet = document.getElementById("budget").value;
 	budgetSet.value = getBudget();
 }
 
@@ -14,10 +15,18 @@ function getBudget(){
 	alert("Grabbed value" + budgetGet);
 	return budgetGet;
 }
-
+*/
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	db.collection('main_budget').add({
 		budget: form.budget.value
 	});
+});
+
+
+docRef.get().then(function(doc){
+	if(doc.exists){
+		var budgetSet = document.getElementById("budget").value;
+		budgetSet.value = doc.data();
+	}
 });
