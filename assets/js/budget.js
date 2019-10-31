c3cdfdda6123624c39b42b899638c88dba586780
JacquesFr <jfrac001@ucr.edu>
2019-10-31 15:47:14 -0700

    testing live

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,29 +1,7 @@
-/*
-function userState() {
-	firebase.auth().onAuthStateChanged(function(user) {
-	  if (user) {
-	    // User is signed in.
-	    var displayName = user.displayName;
-	    var email = user.email;
-	    var emailVerified = user.emailVerified;
-	    var uid = user.uid;
-	    var providerData = user.providerData;
-	    alert("signed in with: " + email);
-	    // ...
-	  } else {
-	  	alert("no one signed in");
-	    // User is signed out.
-	    // ...
-	  }
-	});
-}
-*/
+
 function setBudget(){
-	alert("started db");
-	
 	var budgetInput = document.getElementById("budget").value;
 	//var budgetGet = 420;
-	alert("grabbed value " + budgetInput);
 	var db = firebase.firestore();
 
 	db.collection("My Budget").add({
@@ -36,23 +14,16 @@ function setBudget(){
 		.catch(function(error) {
 		    console.error("Error adding document: ", error);
 	});	
-	alert("Added to collection");
 }
 
 function getBudget(){
-	//var budgetSet = document.getElementById("budget_field").value;
-	//budgetSet.value = getBudget();
-	//comment
-	//var value = document.getString("budget");
-	alert("Get budget called");
+
 	var db = firebase.firestore();
 	db.collection("My Budget").limit(1).get().then(snapshot => {
 		snapshot.forEach(doc => {
 			var budgetSet = document.getElementById("budget");
 			
 			budgetSet.value = doc.data().budget;
-			alert("inside collection");
-			alert(doc.data().budget);
 			if(doc.exists){
 				console.log("Document data:", doc.data());
 		    } else {
@@ -66,55 +37,42 @@ function getBudget(){
 	
 	});
 }
-/*
-function getBudget(){
-	//var budgetSet = document.getElementById("budget").value;
-	//budgetSet.value = getBudget();
-	//comment
-	//var value = document.getString("budget");
 
-	alert("Get budget called");
-	var db = firebase.firestore();
-	db.collection("My Budget").get().then(snapshot => {
-		snapshot.forEach(doc => {
-			alert("inside collection");
-			alert(doc.data().budget);
-			if(doc.exists){
-				console.log("Document data:", doc.data());
-		    } else {
-		        // doc.data() will be undefined in this case
-		        console.log("No such document!");
-		    }
-			}).catch(function(error) {
-			    console.log("Error getting document:", error);
-		});
+function deposit(){
+	//Getting user information
+	alert("deposit()");
+	
+	firebase.auth().onAuthStateChanged(function(user) {
+		alert("inside function");
+		  if (user) {
+		    alert("user found");
+		    var email;
+		    email = user.email;
+		    alert(email);
+		  } else {
+		  	alert("no user");
+		  }
 	});
+	
+
+	
+	
+
+	//Grabbing variables from fields.
+	var depo_budgetInput = document.getElementById("depositAmnt").value;
+	alert("Depositing: " + depo_budgetInput);
+
+	var cat = document.getElementById("drop_deposit_category");
+	var depo_category = cat.options[cat.selectedIndex].text;
+	alert("Category: " + depo_category);
+
+	var depo_description = document.getElementById("deposit_description").value;
+	alert("Description: " + depo_description);
+	
+	
+	
+
+
+	//var db = firebase.firestore();
+
 }
-*/
-
-/*
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	alert("Grabbing Value");
-	db.collection('main_budget').add({
-		budget: form.budget.value
-	});
-});
-docRef.get().then(function(doc){
-	if(doc.exists){
-		var budgetSet = document.getElementById("budget").value;
-		budgetSet.value = doc.data();
-	}
-});
-
-
-
-
-
-
-
-
-//const form = document.querySelector('#main_budget');
-//var docRef =  db.collection("main_budget").doc("latest"); 
-*/
-
