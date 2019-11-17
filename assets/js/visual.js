4caad9960962b397f50a2275bc88c1164d642b3b
JacquesFr <jfrac001@ucr.edu>
2019-11-16 20:28:40 -0800

    Added recommendations to Analytics

--- assets/js/visual.js
+++ assets/js/visual.js
@@ -8,7 +8,6 @@ function visual(){
 	
 	// Create chart instance
 	var chart = am4core.create("chartdiv", am4charts.PieChart);
-	
 	// Add data
 	
 	var user_email = localStorage.getItem("user_Email");
@@ -86,8 +85,81 @@ function visual(){
 		}).catch(function(error) {
 		    console.log("Error getting document:", error);
 	});
-	
-	
+
 	}); // end am4core.ready()
 
+	am4core.ready(function() {
+									
+	// Themes begin
+	am4core.useTheme(am4themes_dark);
+	am4core.useTheme(am4themes_animated);
+	// Themes end
+	
+	// Create chart instance
+	var chart = am4core.create("chartdiv2", am4charts.PieChart);
+	// Add data
+	
+	var user_email = localStorage.getItem("user_Email");
+	var overallBudget;
+	
+	var db = firebase.firestore();
+	
+	db.collection(user_email).doc("Budget").get().then(function(doc) {
+		overallBudget = doc.data().overallBudget;
+
+		if(doc.exists){
+			chart.data = [ {
+			"category": "Entertainment",
+			"amount": overallBudget * 0.10
+			}, {
+			"category": "Housing",
+			"amount": overallBudget * 0.35
+			}, {
+			"category": "Utilities",
+			"amount": overallBudget * 0.05
+			}, {
+			"category": "Food",
+			"amount": overallBudget * 0.20
+			}, {
+			"category": "Transportation",
+			"amount": overallBudget * 0.05
+			}, {
+			"category": "Education",
+			"amount": overallBudget * 0.10
+			}, {
+			"category": "Loan Repayment",
+			"amount": overallBudget * 0.05
+			}, {
+			"category": "Child Care",
+			"amount": overallBudget * 0.05
+			}, {
+			"category": "Savings",
+			"amount": overallBudget * 0.05
+			} ];
+			
+			// Add and configure Series
+			var pieSeries = chart.series.push(new am4charts.PieSeries());
+			pieSeries.dataFields.value = "amount";
+			pieSeries.dataFields.category = "category";
+			pieSeries.slices.template.stroke = am4core.color("#fff");
+			pieSeries.slices.template.strokeWidth = 1;
+			pieSeries.slices.template.strokeOpacity = 1;
+			
+			// This creates initial animation
+			pieSeries.hiddenState.properties.opacity = 1;
+			pieSeries.hiddenState.properties.endAngle = -90;
+			pieSeries.hiddenState.properties.startAngle = -90;
+	    } else {
+	        // doc.data() will be undefined in this case
+	        console.log("No such document!");
+	    }
+
+		}).catch(function(error) {
+		    console.log("Error getting document:", error);
+	});
+
+	}); // end am4core.ready()
+
+	
 }
+
