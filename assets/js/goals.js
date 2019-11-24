7ebbff05aa8b2a727c4768d82d7bd48cb4735b96
ASHTM15 <amcda005@ucr.edu>
2019-11-23 20:03:43 -0800

    generating html statement when user meets their savings req

--- assets/js/goals.js
+++ assets/js/goals.js
@@ -23,12 +23,24 @@ function setGoal(){
     alert("You Have Set A Budget Goal!");
 }//function
 
+const goal1_Notification = document.querySelector('#goals1_tag');
+
+//the getGoals function will be called when the goals page is loaded
 function getGoals(){
+    ////Generate An html statement if the user has met their goal reqs
+    var newDiv = document.createElement("h3");
+    var newContent = document.createTextNode("You have the savings required for Goal #1 !");
+    newDiv.appendChild(newContent);
+    var element1 = document.getElementById("goal1");
+    element1.appendChild(newDiv);
+  ///
+
     var db = firebase.firestore();
     var user_email = localStorage.getItem("user_Email");
     var goal_deposit = document.getElementById("deposit_budget_goal");
     var goalPercentage =  document.getElementById("percentage_goal");
     var goalDescription = document.getElementById("goal_description");
+    
     db.collection(user_email).doc("Budget").collection("goals").doc("Goal_1").get().then(function(doc) {
         goal_deposit.value = doc.data().GoalAmount;
         goalPercentage.value = doc.data().goalPercentage1;
@@ -42,9 +54,12 @@ function getGoals(){
 
 		}).catch(function(error) {
 		    console.log("Error getting document:", error);
-	});
+    });
 }
-/*
+    
 
+/*
+    
+    //
 		
 */
