03b3ddd7e03de9c5de9061d4ff5ea68eb6085941
ASHTM15 <amcda005@ucr.edu>
2019-11-26 10:54:33 -0800

    writing second purchase button for goals

--- assets/js/goals.js
+++ assets/js/goals.js
@@ -223,7 +223,32 @@ function getGoals(){
     }); //first db.collection call
 
     function purchase2(){
-        alert("Purchased2");
+        alert("Purchased1");
+         db.collection(user_email).doc("Budget").get().then(function(doc) {
+
+            savingsAmount = doc.data().savings;
+            alert("Before " + savingsAmount);
+
+            db.collection(user_email).doc("Budget").collection("goals").doc("Goal_1").get().then(function(doc) {
+                amount = doc.data().GoalAmount;
+
+                savingsAmount = savingsAmount - amount;
+                alert("After: " + savingsAmount);
+
+                db.collection(user_email).doc("Budget").update({
+                    savings: savingsAmount
+
+                });
+
+                db.collection(user_email).doc("Budget").collection("goals").doc("Goal_1").set({
+                     GoalAmount: 999999,
+                     GoalDescription1: "Enter New Goal",
+                     goalPercentage1: 999999,
+                });
+
+
+            });
+        });
         
     }
 
