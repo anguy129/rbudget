2272fe53136b4b04c9ad1dd4e6335cfc2547de30
anguy129 <anguy129@ucr.edu>
2019-11-25 18:26:39 -0800

    update visual

--- assets/js/visual.js
+++ assets/js/visual.js
@@ -104,120 +104,6 @@ function visual(){
 	
 	var db = firebase.firestore();
 
-	db.collection(user_email).doc("Budget").get().then(function(doc) {
-			overallBudget = doc.data().overallBudget;
-			selfRecommended = doc.data().selfRecommend;
-			alert(selfRecommended);
-			alert(overallBudget);
-			if(selfRecommended == 0){
-				chart.data = [ {
-					"category": "Entertainment",
-					"amount": overallBudget * 0.10
-					}, {
-					"category": "Housing",
-					"amount": overallBudget * 0.35
-					}, {
-					"category": "Utilities",
-					"amount": overallBudget * 0.05
-					}, {
-					"category": "Food",
-					"amount": overallBudget * 0.20
-					}, {
-					"category": "Transportation",
-					"amount": overallBudget * 0.05
-					}, {
-					"category": "Education",
-					"amount": overallBudget * 0.10
-					}, {
-					"category": "Loan Repayment",
-					"amount": overallBudget * 0.05
-					}, {
-					"category": "Child Care",
-					"amount": overallBudget * 0.05
-					}, {
-					"category": "Savings",
-					"amount": overallBudget * 0.05
-					} ];
-
-			}
-			else{
-
-				db.collection(user_email).doc("Budget").get().then(function(doc) {
-					overallBudget = doc.data().overallBudget;
-					selfRecommended = doc.data().selfRecommend;
-					alert(selfRecommended);
-					alert(overallBudget);
-					
-					housing = housing / 100;
-					food = food / 100;
-					utilities = utilities / 100;
-					savings = savings / 100;
-					education = education / 100;
-					entertainment = entertainment / 100;
-					transportation = transportation / 100;
-					loans = loans / 100;
-					childCare = childCare / 100;
-
-					chart.data = [ {
-					"category": "Entertainment",
-					"amount": overallBudget * entertainment
-					}, {
-					"category": "Housing",
-					"amount": overallBudget * housing
-					}, {
-					"category": "Utilities",
-					"amount": overallBudget * utilities
-					}, {
-					"category": "Food",
-					"amount": overallBudget * food
-					}, {
-					"category": "Transportation",
-					"amount": overallBudget * transportation
-					}, {
-					"category": "Education",
-					"amount": overallBudget * education
-					}, {
-					"category": "Loan Repayment",
-					"amount": overallBudget * loans
-					}, {
-					"category": "Child Care",
-					"amount": overallBudget * childCare
-					}, {
-					"category": "Savings",
-					"amount": overallBudget * savings
-					} ];
-
-				}).catch(function(error) {
-			    console.log("Error getting document:", error);
-				}); //nested db.collection call
-
-			}
-
-			var pieSeries = chart.series.push(new am4charts.PieSeries());
-			pieSeries.dataFields.value = "amount";
-			pieSeries.dataFields.category = "category";
-			pieSeries.slices.template.stroke = am4core.color("#fff");
-			pieSeries.slices.template.strokeWidth = 1;
-			pieSeries.slices.template.strokeOpacity = 1;
-			
-			// This creates initial animation
-			pieSeries.hiddenState.properties.opacity = 1;
-			pieSeries.hiddenState.properties.endAngle = -90;
-			pieSeries.hiddenState.properties.startAngle = -90;
-
-
-
-		console.log("Cached document data:", doc.data());
-	}).catch(function(error) {
-	    console.log("Error getting cached document:", error);
-	}); //first db.collection call
-
-	}); // end am4core.ready()
-	
-
-
-	/*
-
 	db.collection(user_email).doc("Recommendations").get().then(function(doc) {
     // Document was found in the cache. If no cached document exists,
     // an error will be returned to the 'catch' block below.
@@ -235,8 +121,7 @@ function visual(){
 		db.collection(user_email).doc("Budget").get().then(function(doc) {
 			overallBudget = doc.data().overallBudget;
 			selfRecommended = doc.data().selfRecommend;
-			alert(selfRecommended);
-			alert(overallBudget);
+			//alert(selfRecommended);
 			
 			housing = housing / 100;
 			food = food / 100;
@@ -250,7 +135,6 @@ function visual(){
 
 			if(doc.exists){
 				if(selfRecommended == 0){
-					alert("Inside if statement");
 					chart.data = [ {
 					"category": "Entertainment",
 					"amount": overallBudget * 0.10
@@ -338,7 +222,7 @@ function visual(){
 	    console.log("Error getting cached document:", error);
 	}); //first db.collection call
 
-	*/
+	}); // end am4core.ready()
 
 	am4core.ready(function() {
 									
@@ -351,10 +235,10 @@ function visual(){
     	var chart = am4core.create("chartdiv3", am4charts.XYChart);
     
     	// Title
-    	//var title = chart.titles.push(new am4core.Label());
-    	//title.text = "Over/Under";
-    	//title.fontSize = 25;
-    	//title.marginBottom = 15;
+    	var title = chart.titles.push(new am4core.Label());
+    	title.text = "Over/Under";
+    	title.fontSize = 25;
+    	title.marginBottom = 15;
 	
 		// Add data		
 		var user_email = localStorage.getItem("user_Email");
@@ -474,8 +358,10 @@ function visual(){
 				var positiveColor = interfaceColors.getFor("positive");
 				var negativeColor = interfaceColors.getFor("negative");
 				
-				createSeries("negative", "Under", negativeColor);
-				createSeries("positive", "Over", positiveColor);
+				createSeries("negative2", "Unlikely", negativeColor.lighten(0.5));
+				createSeries("negative1", "Never", negativeColor);
+				createSeries("positive1", "Sometimes", positiveColor.lighten(0.5));
+				createSeries("positive2", "Very often", positiveColor);
 			} else {
 				// doc.data() will be undefined in this case
 				console.log("No such document!");
@@ -484,6 +370,6 @@ function visual(){
 			}).catch(function(error) {
 				console.log("Error getting document:", error);
 		});
-
+	
 		}); // end am4core.ready()
 }
