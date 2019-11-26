2bfae762e5f75b99f5d144367496d0a781d640b2
ASHTM15 <amcda005@ucr.edu>
2019-11-26 11:30:44 -0800

    Reordered code for recommendations

--- assets/js/visual.js
+++ assets/js/visual.js
@@ -86,9 +86,131 @@ function visual(){
 		    console.log("Error getting document:", error);
 	});
 
-	}); // end am4core.ready()
+	}); // end am4core.ready()	
 
 	am4core.ready(function() {
+									
+		// Themes begin
+		am4core.useTheme(am4themes_dark);
+		am4core.useTheme(am4themes_animated);
+    	// Themes end
+    
+    	// Create chart instance
+    	var chart = am4core.create("chartdiv3", am4charts.XYChart);
+    
+    	 // Title
+    var title = chart.titles.push(new am4core.Label());
+    title.text = "Research tools used by students";
+    title.fontSize = 25;
+    title.marginBottom = 15;
+    
+    // Add data
+    chart.data = [{
+      "category": "Search engines",
+      "negative": -0.1,
+      "positive": 94
+    }, {
+      "category": "Online encyclopedias",
+      "negative": -2,
+      "positive": 75
+    }, {
+      "category": "Peers",
+      "negative": -2,
+      "positive": 42
+    }, {
+      "category": "Social media",
+      "negative": -2,
+      "positive": 52
+    }, {
+      "category": "Study guides",
+      "negative": -6,
+      "positive": 41
+    }, {
+      "category": "News websites",
+      "negative": -3,
+      "positive": 25
+    }, {
+      "category": "Textbooks",
+      "negative": -5,
+      "positive": 18
+    }, {
+      "category": "Librarian",
+      "negative": -14,
+      "positive": 16
+    }, {
+      "category": "Printed books",
+      "negative": -9,
+      "positive": 12
+    }, {
+      "category": "Databases",
+      "negative": -18,
+      "positive": 17
+    }, {
+      "category": "Student search engines",
+      "negative": -17,
+      "positive": 10
+    }];
+    
+    
+    // Create axes
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.inversed = true;
+    categoryAxis.renderer.minGridDistance = 20;
+    categoryAxis.renderer.axisFills.template.disabled = false;
+    categoryAxis.renderer.axisFills.template.fillOpacity = 0.05;
+    
+    
+    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+    valueAxis.min = -100;
+    valueAxis.max = 100;
+    valueAxis.renderer.minGridDistance = 50;
+    valueAxis.renderer.ticks.template.length = 5;
+    valueAxis.renderer.ticks.template.disabled = false;
+    valueAxis.renderer.ticks.template.strokeOpacity = 0.4;
+    valueAxis.renderer.labels.template.adapter.add("text", function(text) {
+      return text == "Male" || text == "Female" ? text : text + "%";
+    })
+    
+    // Legend
+    chart.legend = new am4charts.Legend();
+    chart.legend.position = "right";
+    chart.legend.width = 50;
+    
+    // Use only absolute numbers
+    chart.numberFormatter.numberFormat = "#.#s";
+    
+    // Create series
+    function createSeries(field, name, color) {
+      var series = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.valueX = field;
+      series.dataFields.categoryY = "category";
+      series.stacked = true;
+      series.name = name;
+      series.stroke = color;
+      series.fill = color;
+      
+      var label = series.bullets.push(new am4charts.LabelBullet);
+      label.label.text = "{valueX}%";
+      label.label.fill = am4core.color("#fff");
+      label.label.strokeWidth = 0;
+      label.label.truncate = false;
+      label.label.hideOversized = true;
+      label.locationX = 0.5;
+      return series;
+    }
+    
+    var interfaceColors = new am4core.InterfaceColorSet();
+    var positiveColor = interfaceColors.getFor("positive");
+    var negativeColor = interfaceColors.getFor("negative");
+    
+    createSeries("negative", "Under", negativeColor);
+    createSeries("positive", "Over", positiveColor);
+    
+    }); // end am4core.ready()
+
+    am4core.ready(function() {
 									
 	// Themes begin
 	am4core.useTheme(am4themes_dark);
@@ -220,127 +342,4 @@ function visual(){
 	}); //first db.collection call
 
 	}); // end am4core.ready()
-	
-
-	am4core.ready(function() {
-									
-		// Themes begin
-		am4core.useTheme(am4themes_dark);
-		am4core.useTheme(am4themes_animated);
-    	// Themes end
-    
-    	// Create chart instance
-    	var chart = am4core.create("chartdiv3", am4charts.XYChart);
-    
-    	 // Title
-    var title = chart.titles.push(new am4core.Label());
-    title.text = "Research tools used by students";
-    title.fontSize = 25;
-    title.marginBottom = 15;
-    
-    // Add data
-    chart.data = [{
-      "category": "Search engines",
-      "negative": -0.1,
-      "positive": 94
-    }, {
-      "category": "Online encyclopedias",
-      "negative": -2,
-      "positive": 75
-    }, {
-      "category": "Peers",
-      "negative": -2,
-      "positive": 42
-    }, {
-      "category": "Social media",
-      "negative": -2,
-      "positive": 52
-    }, {
-      "category": "Study guides",
-      "negative": -6,
-      "positive": 41
-    }, {
-      "category": "News websites",
-      "negative": -3,
-      "positive": 25
-    }, {
-      "category": "Textbooks",
-      "negative": -5,
-      "positive": 18
-    }, {
-      "category": "Librarian",
-      "negative": -14,
-      "positive": 16
-    }, {
-      "category": "Printed books",
-      "negative": -9,
-      "positive": 12
-    }, {
-      "category": "Databases",
-      "negative": -18,
-      "positive": 17
-    }, {
-      "category": "Student search engines",
-      "negative": -17,
-      "positive": 10
-    }];
-    
-    
-    // Create axes
-    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "category";
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.inversed = true;
-    categoryAxis.renderer.minGridDistance = 20;
-    categoryAxis.renderer.axisFills.template.disabled = false;
-    categoryAxis.renderer.axisFills.template.fillOpacity = 0.05;
-    
-    
-    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-    valueAxis.min = -100;
-    valueAxis.max = 100;
-    valueAxis.renderer.minGridDistance = 50;
-    valueAxis.renderer.ticks.template.length = 5;
-    valueAxis.renderer.ticks.template.disabled = false;
-    valueAxis.renderer.ticks.template.strokeOpacity = 0.4;
-    valueAxis.renderer.labels.template.adapter.add("text", function(text) {
-      return text == "Male" || text == "Female" ? text : text + "%";
-    })
-    
-    // Legend
-    chart.legend = new am4charts.Legend();
-    chart.legend.position = "right";
-    chart.legend.width = 50;
-    
-    // Use only absolute numbers
-    chart.numberFormatter.numberFormat = "#.#s";
-    
-    // Create series
-    function createSeries(field, name, color) {
-      var series = chart.series.push(new am4charts.ColumnSeries());
-      series.dataFields.valueX = field;
-      series.dataFields.categoryY = "category";
-      series.stacked = true;
-      series.name = name;
-      series.stroke = color;
-      series.fill = color;
-      
-      var label = series.bullets.push(new am4charts.LabelBullet);
-      label.label.text = "{valueX}%";
-      label.label.fill = am4core.color("#fff");
-      label.label.strokeWidth = 0;
-      label.label.truncate = false;
-      label.label.hideOversized = true;
-      label.locationX = 0.5;
-      return series;
-    }
-    
-    var interfaceColors = new am4core.InterfaceColorSet();
-    var positiveColor = interfaceColors.getFor("positive");
-    var negativeColor = interfaceColors.getFor("negative");
-    
-    createSeries("negative", "Under", negativeColor);
-    createSeries("positive", "Over", positiveColor);
-    
-    }); // end am4core.ready()
 }
